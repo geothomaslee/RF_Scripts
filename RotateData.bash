ba0556b7329958c628e387630c31511edef46b2b
geothomaslee <117687003+geothomaslee@users.noreply.github.com>
2022-11-07 14:49:19 -0700

    Update RotateData.bash

--- RotateData.bash
+++ RotateData.bash
@@ -1,29 +1,30 @@
 #!/bin/bash 
 
+# This script assumes 3 component seismic data per station, sorted by event
+# An example file structure is included in the GitHub repository
+
 StartDir="${PWD}" # Defines the starting directory as location of this script
-echo "Beginning in $StartDir"
 DirPref="Event_" # Defines the prefix of the event directories
 StatList="StationList.txt" # Defines the station list
 
 #StatList requires a list of the 3 (or 2 or 4) digit station code for each station within the desired network
+#Example: Network ZR from 2015-2018 has stations W1A, W1B, etc.
 #This code assumes all stations are within the same network, will require some tweaking otherwise
 
 NumStations=$(wc -l < $StatList) # Defines number of stations
-echo "$NumStations Stations Found In List"
+echo "$NumStations Stations Found In Station List"
 
-for dir in `ls -ad ${DirPref}*`; do # Loops through event directories ##LOOP1
-    cd $dir # Enters the event directory
-    echo "Working on $dir"
+for event in `ls -ad ${DirPref}*`; do 
+    cd $event 
+    echo "Working on $event"
     
-    for i in $(seq 1 $NumStations); do ##LOOP2
+    for i in $(seq 1 $NumStations); do 
 	CurStat=$(awk 'NR=='$i'{ print; exit }' ../$StatList) # Reads the i'th line of the station list
-	CurRotFile="$CurStat.rotate" # Creates variable for current rotation file before checking if it actually needs to be made to avoid breaking checks later in code
+	CurRotFile="$CurStat.rotate" # Defines rotation file for current station
 
+	doSACFilesExist=$(find . -maxdepth 1 -type f -name "*$CurStat*.SAC") # Searches for SAC files within event directory. By default, will not search subdirectories
 
-	doSACFilesExist=$(find . -maxdepth 1 -type f -name "*$CurStat*.SAC") # Set var to output of a find command to get thse dumb wildcards to work
-
-	if [ -z "$doSACFilesExist" ]; then ##LOOP5 Only creates rotation files for stations with SAC files
-	    #echo "No SAC files found for Station $CurStat in $dir" De-comment for debug
+	if [ -z "$doSACFilesExist" ]; then # Only creates rotation files for stations with SAC files
 	    :
         else
 	cat > $CurRotFile <<+ # Has to be indented to left to write file properly
@@ -37,27 +38,25 @@ w $CurStat.z
 q
 +
  
-	if [ -f "$CurRotFile" ]; then ##LOOP3 Checks if rotation file was properly created, only if data exists for the station
-           echo "Successfully created rotation file for Station $CurStat in $dir"
-        else
-           echo "Failed to create rotation file for Station $CurStat in $dir"
-	fi ##LOOP3
+		if [ -f "$CurRotFile" ]; then
+           		echo "Successfully created rotation file for Station $CurStat in $dir"
+        	else
+           		echo "Failed to create rotation file for Station $CurStat in $dir"
+		fi 
 
+ 		if [ -f $CurRotFile ]; then # Only tries to run the current rotation file if it exists 
+			sac ./$CurRotFile 
+   		fi 
 
- if [ -f $CurRotFile ]; then # Only tries to run the current rotation file if it exists ##LOOP6
-	sac ./$CurRotFile 
-    fi ##LOOP6
-
-    if [[ -f "$CurStat.r" && "$CurStat.t" && "$CurStat.z" ]]; then ##LOOP4
-           echo "Successfully rotated data for Station $CurStat in $dir"
-	   rm $CurRotFile # Deletes rotation file for better organization, KEEP IF DEBUGGING
-        else
-           echo "Failed to rotate data for Station $CurStat in $dir"
-    fi ##LOOP4
-
-        fi ##LOOP5 Closes the if statement that checks if data exists for the current station
-
-    done ##LOOP2
+    		if [[ -f "$CurStat.r" && "$CurStat.t" && "$CurStat.z" ]]; 
+		then 
+          		echo "Successfully rotated data for Station $CurStat in $dir"
+	   		rm $CurRotFile # Deletes rotation file - COMMENT IF DEBUGGING
+        	else
+           		echo "Failed to rotate data for Station $CurStat in $dir"
+   		fi
+    	fi
+    done 
     
-    cd ../ # Returns to main data directory
-done ##LOOP1
+    cd $StartDir # Returns to main data directory
+done 
