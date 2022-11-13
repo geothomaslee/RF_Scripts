21fb2e42a3917d4dbd76783bcfb3cc2eb889934b
geothomaslee <117687003+geothomaslee@users.noreply.github.com>
2022-11-13 15:05:11 -0700

    Add files via upload

--- Processing/FindSegmented.bash
+++ Processing/FindSegmented.bash
@@ -4,6 +4,12 @@ DirPref="Event_" # Common prefix for all event directories
 StartDir=`pwd`
 StatList="$StartDir/StationList.txt"
 
+## COMPONENT NAMES ##
+Comp1="HH1" # First horizontal component code
+Comp2="HH2" # Second horizontal component code
+CompVert="HHZ" # Vertical component code
+
+
 if [ -d $StartDir/Missing_Components ];
 then
 	:
@@ -16,6 +22,12 @@ then
 	:
 else
 	mkdir $StartDir/Segmented_Components
+fi
+
+if [ -d $StartDir/Missing_And_Excess_Components ];
+then
+	mkdir $StartDir/Missing_And_Excess_Components
+else
 fi
 
 StatCount=$(wc -l < $StatList)
@@ -40,18 +52,32 @@ for event in `ls -ad ${DirPref}*`; do
 			NumComps=$(wc -l < FindSegmentedTempFile.txt)
 			if [[ $NumComps == "3" ]];
 			then
-				:
+				NumComp1=$(grep -c $Comp1 FindSegmentedTempFile.txt)
+				NumComp2=$(grep -c $Comp2 FindSegmentedTempFile.txt)
+				NumCompVert=$(grep -c $CompVert FindSegmentedTempFile.txt)
+
+				if [[ $NumComp1 -gt "1" ]] || [[ $NumComp2 -gt "1" ]] || [[ $NumCompVert -gt "1" ]];
+				then
+					echo "$CurStat in $event has excess and missing components"
+					if [ -d $StartDir/Missing_And_Excess_Components/$event ];
+					then
+						:
+					else
+						mkdir $StartDir/Missing_And_Excess_Components/$event
+					fi
+					mv $Comps $StartDir/Missing_And_Excess_Components/$event
+				fi
 			else
 				if [[ $NumComps -lt "3" ]];
 				then
 					echo "$CurStat in $event is missing components"
-					if [ -d $StartDir/Missing_Components/$event];
+					if [ -d $StartDir/Missing_Components/$event ];
 					then
 						:
 					else
 						mkdir $StartDir/Missing_Components/$event
 					fi 
-					cp $Comps $StartDir/Missing_Components/$event
+					mv $Comps $StartDir/Missing_Components/$event
 				fi
 				
 				if [[ $NumComps -gt "3" ]];
@@ -64,9 +90,11 @@ for event in `ls -ad ${DirPref}*`; do
 					else
 						mkdir $StartDir/Segmented_Components/$event
 					fi
-					cp $Comps $StartDir/Segmented_Components/$event
+					mv $Comps $StartDir/Segmented_Components/$event
 				fi
 			fi
+
+					
 		fi
 
 		cat /dev/null > FindSegmentedTempFile.txt
