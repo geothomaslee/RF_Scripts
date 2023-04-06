1972928f95cc9609b16a0d4df11f7c48b179a51f
geothomaslee <117687003+geothomaslee@users.noreply.github.com>
2023-04-06 11:02:24 -0700

    Add files via upload
    - Now must specify file extension for radial and tangential inputs
    - Adds Gaussian into the name of the RFs to make testing different Gaussians easier

--- ReceiverFunctions/IterdeconByEvents.bash
+++ ReceiverFunctions/IterdeconByEvents.bash
@@ -1,19 +1,22 @@
 #!/bin/bash 
 
 ## IMPORTANT BEFORE USE ##
-# This script assumes you have an alias set up for iterdecon in your .bashrc file. If this is not the case, edit lines 78 and 80 to replace iterdecon with the location of the executable
+# This expects radial and tangential input files that are named as Station.Suffix where Suffix (NOT .SUFFIX) is defined in the parameters r_file_input and t_file_input. This is intended to add flexibility for testing different filtering parameters.
+# This script assumes you have an alias set up for iterdecon in your .bashrc file. If this is not the case, edit lines 85 and 87 to replace iterdecon with the location of the executable
 # Iterdecon breaks if you feed it more than 4096 points and the error message does not communicate the actual problem. Make sure your files have <4096 points. Use FilterData to quickly downsample if you need that.
 
 ## ITERDECON PARAMETERS ##
 infile="input.iterdecon" # Defines the input file
 iterations="400"
-phaseshift="30" # Time difference between P-arrival and start of file
+phaseshift="10" # Time difference between P-arrival and start of file
 delta=".001"
-gaussian="2.5"
+gaussian="5"
 wvforms="1"
+r_file_input="bp.0.05.2.r" 
+t_file_input="bp.0.05.2.t"
+z_file_input="bp.0.05.2.z"
 
 iterdecon="/usr/local/Owens_Software/bin/iterdecon_batch" ## Iterdecon executable location
-
 
 LogOutput="1" # DEFAULT 1
 outfile="output.iterdecon" 
@@ -53,18 +56,18 @@ for event in `ls -ad ${DirPref}*`; do # Loops through event directories
 	if [ -z "$doRotatedFilesExist" ]; then 
 	    :
         else
-		echo "$CurStat.r" >> $infile # Defines radial input file
-		echo "$CurStat.z" >> $infile # Defines vertical input file
-		echo "$event.$CurStat.itr" >> $infile # Defines the radial output file
+		echo "$CurStat.$r_file_input" >> $infile # Defines radial input file
+		echo "$CurStat.$z_file_input" >> $infile # Defines vertical input file
+		echo "$event.$CurStat.g.$gaussian.itr" >> $infile # Defines the radial output file
 		echo $iterations >> $infile
 		echo $phaseshift >> $infile
 		echo $delta >> $infile
 		echo $gaussian >> $infile
 		echo $wvforms >> $infile
 		echo "0" >> $infile
-		echo "$CurStat.t" >> $infile # Defines radial input file
-		echo "$CurStat.z" >> $infile # Defines vertical  input file
-		echo "$event.$CurStat.itt" >> $infile # Defines the tangential output file
+		echo "$CurStat.$t_file_input" >> $infile # Defines tangential input file
+		echo "$CurStat.$z_file_input" >> $infile # Defines vertical  input file
+		echo "$event.$CurStat.g.$gaussian.itt" >> $infile # Defines the tangential output file
 		echo $iterations >> $infile
 		echo $phaseshift >> $infile
 		echo $delta >> $infile
